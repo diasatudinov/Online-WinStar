5ed23927a9fe1d57e465214b0a84370e8e221e61
Dias <dias.atudinov@gmail.com>
2024-11-21 17:30:17 +0500

    New project

---
+++ Online WinStar/Online WinStar/Online_WinStarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_WinStarApp.swift
+//  Online WinStar
+//
+//  Created by Dias Atudinov on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Online_WinStarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
