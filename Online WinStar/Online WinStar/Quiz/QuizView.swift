799006e4236f99d44e6a7d5382dcbf2fcb59587d
Dias <dias.atudinov@gmail.com>
2024-11-22 19:12:48 +0500

    new files

---
+++ Online WinStar/Online WinStar/Quiz/QuizView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  Online WinStar
+//
+//  Created by Dias Atudinov on 22.11.2024.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizView()
+}
